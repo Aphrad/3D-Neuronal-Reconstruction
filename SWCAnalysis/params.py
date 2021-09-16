e2090232ddaa8ffac579497b32ed6cea5b39e60e
Aphrad <wangyj_mail@163.com>
2021-09-16 20:44:31 +0800

    first commit

---
+++ SWCAnalysis/params.py
@@ -0,0 +1,17 @@
+# SWC Indices
+SWC_INDICES = {'id': 0, 'sid': 1, 'x': 2, 'y': 3, 'z': 4, 'radius': 5, 'pid': 6}
+
+# Soma Parent ID
+SOMA_PID = -1
+
+# SWC delimiter
+SWC_DELIMITER = ' '
+
+# Structure ID Dictionary
+SID_DICT = {'soma': 1, 'axon': 2, 'basal dendrite': 3, 'apical dendrite': 4}
+
+# Structure Color Dictionary
+SID_COLORS = {0: 'green', 1: 'black', 2: 'red', 3: 'yellow', 4: 'blue', 5: 'purple',
+              6: 'grey', 7: 'olive', 8: 'gray', 9: 'pink', 10: 'brown', 11: 'cyan',
+              12: 'peru', 13: 'gold', 14: 'indigo', 15: 'crimson', 16: 'silver', 17: 'navy',
+              18: 'lime', 19: 'wheat', 20: 'coral'}
